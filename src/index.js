116fca9b713b597dede2f658e702be63217f1b6b
MrSpunki <36659232+MrSpunki@users.noreply.github.com>
2018-10-06 16:07:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  var zero = 0;
+  
+  for (var i = 0; i <= number; i += base) {
+    
+    var zapas = i;
+    
+    if (i > 0) {
+      var newBaseNumber = []; 
+    
+      while (zapas > 0) {
+        var helpNumber =zapas;
+        zapas = Math.floor(zapas / base);
+        
+        var restNumber = helpNumber - zapas * base;
+        newBaseNumber.push(restNumber);//записываю остатки в массив
+      }
+  
+      newBaseNumber.reverse();//переворачиваю его
+      var stringNumber = newBaseNumber.join('');//преобразую в строку
+      var notStringNumber = Number(stringNumber);//преобразую в число
+      
+      var zeroNumber = notStringNumber;
+      var temp = notStringNumber % 10;
+  
+      while ((zeroNumber % 10) === 0) {
+        zeroNumber = zeroNumber / 10;
+        zero += 1;
+      }
+    }
+  }
+  
+  return zero;
+  
 }
